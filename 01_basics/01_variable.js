447658b6c14d2cf2656c0067fae0f2c3dfbbcc51
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-03-23 16:22:53 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let emailId="shashi@zxc19"
+var accountNumber="123456789"
+
+let accountState;
+
+//accountId=1253 // its invalid u can not change the value which u have put in the cosnt// once its put in const can't be changed
+
+console.log(accountId)
+emailId="shashi@123"
+accountNumber="987456321"
+
+
+console.table([accountId,emailId,accountNumber,accountState])
