f91899496d3f36118ca3a994b69df3f13d0b640e
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-03-24 07:25:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all Js code as newer version
+
+
+//alert(3+3) // we are using nodejs, not browser
+
+// console.log(3+3) console.log("shekhar")
+/* this is not prefered becasue it reduces the code
+    readability  
+*/
+
+let name ="shashi shekhar"
+let age= 18
+let isLoggedIn= true
+
+let state;
+
+
+// number => ranges up to 2 to the power 52--23
+//bigint
+//string =>""
+//boolean =>ture/false
+//null =>standalone value
+// undefined=> undefined hi hai 
+//symbol => unique
+
+//object => null
+
+console.log(typeof age)  // number
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof name) // string
+console.log(typeof null)     // object 
+console.log(typeof undefined) // undefined
