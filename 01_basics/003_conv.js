9e9f07110cd8294eff686838d0d4470b7a551dd7
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-03-25 15:37:53 +0000

    conversion

---
+++ 01_basics/003_conv.js
@@ -0,0 +1,41 @@
+//*********************another operations to be studied**************** */
+
+let value=3
+let negValue= -value
+console.log(negValue) //-3
+
+
+console.log(2+2) //4
+console.log(2-2) //0
+console.log(2*2)//4
+console.log(2/3)//0.666666
+console.log(2%5) //2
+console.log(2**3) //8
+
+let str1 ="hello"
+let str2 = " shahsi shekhar"
+
+let str3=str1+str2
+console.log(str3) // hello shashi shekhar
+
+
+console.log(1+"2") //12
+console.log("1"+2) //12
+console.log("1"+2+3)//123
+console.log(1+2+"3") //33
+//here it it follows the rule of ecca > if first string then all other string if first number then procced with number addition if second is number too 
+
+// console.log(3+4*5/3)// this is not preferred as code readability is reduced 
+
+console.log(true)//true
+console.log(+true)//1
+
+console.log(+"") //0
+
+let num1,num2,num3
+
+num1=num2=num3=2+2 // it make code messy and not readable
+
+let gameCounter =100
+++gameCounter
+console.log(gameCounter) //101
