49c9da432899b4f446f89270505e144b7c01238f
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-03-25 15:28:39 +0000

    more on conversion of the operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score="50abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof score); // string
+console.log(typeof(score));//string
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score) //n 
 console.log(typeof valueInNumber); // it will show it as number but it will print NaN
 console.log(valueInNumber); // NaN not a number will be printed 
 
@@ -14,6 +14,48 @@ console.log(valueInNumber); // NaN not a number will be printed
 let isLoggedIn=1
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn); //true
 
 // 1=> true 0=> false
+
+//*********************another operations to be studied**************** */
+
+let value=3
+let negValue= -value
+console.log(negValue) //-3
+
+
+console.log(2+2) //4
+console.log(2-2) //0
+console.log(2*2)//4
+console.log(2/3)//0.666666
+console.log(2%5) //2
+console.log(2**3) //8
+
+let str1 ="hello"
+let str2 = " shahsi shekhar"
+
+let str3=str1+str2
+console.log(str3) // hello shashi shekhar
+
+
+console.log(1+"2") //12
+console.log("1"+2) //12
+console.log("1"+2+3)//123
+console.log(1+2+"3") //33
+//here it it follows the rule of ecca > if first string then all other string if first number then procced with number addition if second is number too 
+
+// console.log(3+4*5/3)// this is not preferred as code readability is reduced 
+
+console.log(true)//true
+console.log(+true)//1
+
+console.log(+"") //0
+
+let num1,num2,num3
+
+num1=num2=num3=2+2 // it make code messy and not readable
+
+let gameCounter =100
+++gameCounter
+console.log(gameCounter) //101
