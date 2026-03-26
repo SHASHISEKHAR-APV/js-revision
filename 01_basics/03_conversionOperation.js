2f289139e057641dd198ceb8270b7f861d655263
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-03-26 18:00:58 +0000

    jut updating

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -60,3 +60,26 @@ let gameCounter =100
 ++gameCounter
 console.log(gameCounter) //101
 
+// more topics 
+console.log(2>1) // true
+console.log(2<1) //false
+console.log(3<2) // false
+console.log(3>3) // false
+console.log(2==1) // false
+console.log(2!=1)// true
+
+console.log("2"> 1) // true
+console.log("02"<1) // false
+
+
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null<=0) //true
+console.log(null>=0)// true
+
+console.log(undefined==0) //false
+console.log(undefined>0) //false
+console.log(undefined<0) //false
+
+//====   represents strict check on data types whether datatyes are same or different or not 
+console.log("2" ===2) //false
