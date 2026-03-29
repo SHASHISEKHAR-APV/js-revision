b5acd3ee816dfac48fdcdf9b79ab8d77d9ae28a3
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-03-29 17:37:15 +0000

    more for the streak

---
+++ 01_basics/random.js
@@ -0,0 +1,31 @@
+// Variables
+let name = "Arun";
+let age = 25;
+
+// Simple function
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+// Object
+let person = {
+  name: "Arun",
+  age: 25
+};
+
+// Function using object
+function showPerson(p) {
+  console.log("Name: " + p.name);
+  console.log("Age: " + p.age);
+}
+
+// Calling functions
+greet(name);
+showPerson(person);
+
+// Simple calculation function
+function add(a, b) {
+  return a + b;
+}
+
+console.log("Sum: " + add(2, 3));
