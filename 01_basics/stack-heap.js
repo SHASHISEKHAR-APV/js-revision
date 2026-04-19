518a069ca0f09f9c26412d6f4a0439c680eae974
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-04-19 08:44:31 +0000

    v

--- 01_basics/stack-heap.js
+++ 01_basics/stack-heap.js
@@ -11,10 +11,10 @@ console.log(y);       // 20
 
 // 🔹 HEAP (Reference types → Copy by reference)
 
-let a = { val: 1 };   // a -> { val: 1 } (heap)
+let a = { val: 10 };   // a -> { val: 1 } (heap)
 let b = a;            // b -> same object
 
-b.val = 2;            // modifies shared object
+b.val = 20;            // modifies shared object
 
 console.log(a.val);   // 2
 console.log(b.val);   // 2
@@ -22,7 +22,7 @@ console.log(b.val);   // 2
 
 // 🔹 FIX: Create new object (no shared reference)
 
-let c = { val: 1 };       
+let c = { val: 10 };       
 let d = { ...c };     // new copy (shallow)
 
 d.val = 5;
